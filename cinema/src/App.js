608f0f29d4caa0399524093ce1d369bd2487ddfc
Volodymyr <mr.catmanes@gmail.com>
2025-05-13 20:08:11 +0300

    Update routing for site, add header component and use toastcontainer

--- cinema/src/App.js
+++ cinema/src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import Booking from "./components/pages/Booking/Booking";
+import GuestBooking from "./components/pages/GuestBooking/GuestBooking";
+import Auth from "./components/pages/Auth/Auth";
+import Header from "./components/templates/Header/Header";
+import Tickets from "./components/pages/Tickets/Tickets"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/booking/:id" element={<Booking />} />
-    </Routes>
+    <Header />
+    <ToastContainer position="top-right" autoClose={500} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking/:id" element={<Booking />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/guest-booking" element={<GuestBooking />} />
+        <Route path="/tickets" element={<Tickets />} />
+        
+      </Routes>
+    
   </Router>
 );
 
